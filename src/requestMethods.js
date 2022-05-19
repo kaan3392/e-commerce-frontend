a0a86b1d0033600c3158650eb9d335390a7cde3a
kaan3392 <kaanalacali@gmail.com>
2022-05-19 14:58:52 +0300

    ..

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = process.env.NODE_ENV === "development"
 ? "http://localhost:8800/api/"
-: "https://e-commerce-knlcl.herokuapp.com/api";
+: "https://e-commerce-knlcl.herokuapp.com/api/";
 
 
 let TOKEN = "";
