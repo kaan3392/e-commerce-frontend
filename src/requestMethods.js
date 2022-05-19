4fd7a99eeb853227f1c4def3ebba562518559a21
kaan3392 <kaanalacali@gmail.com>
2022-05-19 14:47:47 +0300

    baseurl

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8800/api/";
+const BASE_URL = process.env.NODE_ENV === "development"
+? "http://localhost:8800/api/"
+: "https://social-media-knlcl.herokuapp.com/api";
+
+
 let TOKEN = "";
 const temp = localStorage.getItem("persist:root");
 if (temp) {
