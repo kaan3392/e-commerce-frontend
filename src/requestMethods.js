9509ef39ff48ee6b76c1a8471ade2227a7a589f0
kaan3392 <kaanalacali@gmail.com>
2022-12-13 15:52:04 +0300

    api degisikligi

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = process.env.NODE_ENV === "development"
 ? "http://localhost:8800/api/"
-: "https://e-commerce-knlcl.herokuapp.com/api/";
+: "https://enthusiastic-duck-leotard.cyclic.app/api/";
 
 
 let TOKEN = "";
